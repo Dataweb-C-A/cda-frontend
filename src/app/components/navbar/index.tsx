63effa9f8e8a86a48ce48990e4100b0ac70beab7
Javier <javierdiazt406@gmail.com>
2023-02-19 16:53:27 -0400

    Improving Menu of Navbar

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { BsFillPersonFill } from 'react-icons/bs'
-import { Button, Popover, Text } from '@mantine/core'
+import { Button, Menu, Text } from '@mantine/core'
 import '../../assets/scss/navbar.scss'
 import Sidebar from '../sidebar'
 import AvatarCard from '../avatarCard'
@@ -13,22 +13,19 @@ function Navbar() {
     <nav className="navbar">
       <img src="https://admin.rifa-max.com/static/media/ticket.1e676ae5de33fcd376d5.png" width='89px' height='54px' alt="logo" />
       <div>
-        <Popover width="130px" position="bottom" withArrow={false} shadow="md">
-          <Popover.Target>
+        <Menu shadow="md" width={200}>
+          <Menu.Target>
             <button className="button-user"><BsFillPersonFill className='user-icon' /></button>
-          </Popover.Target>
-          <Popover.Dropdown style={{ marginTop: "10px" }}>
-            <Text size="sm" ta="center">
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            <Menu.Label ta='center' fw={600} fz={14}>
               Opciones
-            </Text>
-            <hr/>
-            <Button color="red" variant="filled" size="sm" style={{ width: "100%"}}>
-              <Text size="sm">
-                Cerrar Sesión
-              </Text>
-            </Button>
-          </Popover.Dropdown>
-        </Popover>
+            </Menu.Label>
+            <Menu.Divider />
+            <Menu.Item color="red" ta='center' fw={600}>Cerrar Sesión</Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
       </div>
       <div className='menu' onClick={() => { setIsOpen(!isOpen) }}>
         <div className={isOpen === true ? 'open' : 'close'}></div>
--- src/app/components/sidebar/index.tsx
+++ src/app/components/sidebar/index.tsx
@@ -3,13 +3,17 @@ import { Drawer } from '@mantine/core'
 
 type SidebarProps = {
   profile?: React.ReactNode
-  children?: React.ReactNode
   isAuth?: boolean | false
   opened?: boolean
 }
 
 export default function Sidebar({profile, isAuth, opened}: SidebarProps) {
   const [isOpen, setIsOpen] = useState(opened)
+  const elements2 = [{
+    name: "Prueba"
+  }, {
+    name: "Prueba2"
+  }]
   return (
     <>
       {
@@ -22,6 +26,9 @@ export default function Sidebar({profile, isAuth, opened}: SidebarProps) {
             size="md"
           >
             {profile}
+          <ul>
+            
+          </ul>
           </Drawer>
         ) : null
       }
