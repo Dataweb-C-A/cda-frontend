110998b78f8729d600eef49b05ff9daae5253dcd
Javier <javierdiazt406@gmail.com>
2023-02-25 19:00:57 -0400

    onStepClick disabled

--- src/app/components/formModal/index.tsx
+++ src/app/components/formModal/index.tsx
@@ -18,7 +18,7 @@ function FormModal({opened, onClose}: Props) {
         title="Agregar Rifas"
         size="xl"
       >
-        <Stepper active={active} onStepClick={setActive} breakpoint="sm">
+        <Stepper active={active} breakpoint="sm">
           <Stepper.Step label="Paso 1" description="Datos de la rifa">
             <Title order={4} mt={-10} c="blue" mb="md" ta="center">
               Llena los datos de la rifa
