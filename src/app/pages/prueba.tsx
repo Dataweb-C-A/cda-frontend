9151d41462d9349922a8a920f0fe83566c5b613a
Javier <javierdiazt406@gmail.com>
2023-03-01 12:15:43 -0400

    Manual

--- src/app/pages/prueba.tsx
+++ src/app/pages/prueba.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
-import { Grid, Card, Text, Button, Title, Collapse, Breadcrumbs, Anchor, Chip, CloseButton, Group } from '@mantine/core'
+import React, { useState, useEffect } from 'react'
+import { Grid, Card, Text, Button, Title, Chip, Kbd, Modal } from '@mantine/core'
 import AccordionList from '../components/accordionList'
 import { rifaData } from '../assets/data/rifaData'
 import FormModal from '../components/formModal'
-import Chips from '../components/chip'
 
 interface RiferosProps {
   data: {
@@ -36,18 +35,53 @@ interface RifaAccordionProps {
 
 function RifaAccordion({}: RifaAccordionProps) {
   const [ formModal, setFormModal ] = useState(false)
+  const [ helpModal, setHelpModal ] = useState(false)
   const [ openFilter, setOpenFilter ] = useState(false)
-  const items = [
-    { title: 'Filtrar'},
-    { title: 'Todas'}
-  ].map((item, index) => (
-    <Text key={index}>
-      {item.title}
-    </Text>
-  ));
+
+  useEffect(() => {
+    const keyDownHandler = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === 'm' || event.key === 'M') {
+        setHelpModal(true)
+      }
+    }
+    
+    document.addEventListener('keydown', keyDownHandler)
+    
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler)
+    }
+  }, [])
 
   return (
     <>
+      {
+        helpModal && (
+          <Modal
+            opened={helpModal}
+            onClose={() => setHelpModal(false)}
+            title="Ayuda"
+            size="sm"
+          >
+            <Text>
+              <hr/>
+              <p>En esta sección se muestran las rifas, puedes filtrar por categoria y tipo.<br/></p>
+              <p>
+                <hr/>
+                <br/><Text ta="center"><b>Tipos:</b> Normal y Especial</Text><br/>
+                <b>Normal:</b> Rifas que contienen números o comodines y sus numeros de tickets no son mayor a 12.<br/><br/>
+                <b>Especial:</b> Rifas que contienen números o comodines y sus numeros de tickets son mayor a 12.
+              </p>
+              <p>
+                <Text ta="center"><b>Categorias:</b> Normal, Triples y Terminal</Text><br/>
+                <b>Normal:</b> Rifas en las cuales sus numeros van desde el 100 hasta el 999.<br/><br/>
+                <b>Triple:</b> Rifas en las cuales sus numeros van desde el 010 hasta el 099.<br/><br/>
+                <b>Terminal:</b> Rifas en las cuales sus numeros van desde el 001 hasta el 009.
+              </p>
+
+            </Text>
+          </Modal>
+        )
+      }
       <Card 
         mx={15}
         shadow={
@@ -60,7 +94,10 @@ function RifaAccordion({}: RifaAccordionProps) {
               Rifas
               <Text fw={300} fz={20}>Estado de las Rifas mensuales</Text>
               <Card>
-              <Chip color='blue' my={-10} onClick={() => setOpenFilter(!openFilter)} checked={openFilter}>Filtrar</Chip>
+                <div style={{ display: 'flex', alignItems: 'center', gap: '5px'}}>
+                  <Chip color='blue' my={-10} onClick={() => setOpenFilter(!openFilter)} checked={openFilter}>{ openFilter === true ? 'Filtrar' : 'Todas'}</Chip>
+                  <Kbd ml={10} mt={10}>Ctrl + M</Kbd>
+                </div>
               </Card>
             </Title>
             {
@@ -72,9 +109,9 @@ function RifaAccordion({}: RifaAccordionProps) {
               </Title>
             <div style={{ display: 'flex', alignItems: 'center', gap: '5px', marginTop:'-15px', marginBottom:'10px' }}>
               <Text fw={300} fz={20} mt={5}>
-                Según tipo:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Tipo:
               </Text>
-              <Chip color="blue" variant="outline" size='sm' mt={10}>
+              <Chip color="blue" variant="outline" size='sm' mt={10} ml={55.5}>
                 Normal
               </Chip>
               <Chip color="blue" variant="outline" size='sm' mt={10}>
@@ -83,7 +120,7 @@ function RifaAccordion({}: RifaAccordionProps) {
             </div>
             <div style={{ display: 'flex', alignItems: 'center', gap: '5px', marginTop:'-15px', marginBottom:'10px' }}>
               <Text fw={300} fz={20} mt={5}>
-                Según números: &nbsp;
+                Categoria: &nbsp;
               </Text>
               <Chip color="blue" variant="outline" size='sm' mt={10}>
                 Normal
