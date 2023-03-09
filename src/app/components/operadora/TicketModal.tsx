1d8989d03891ec91ba772b310d149bcbacb53dfb
Javier <javierdiazt406@gmail.com>
2023-03-09 03:32:42 -0400

    Ticket modal added

---
+++ src/app/components/operadora/TicketModal.tsx
@@ -0,0 +1,36 @@
+import { Card, Text } from '@mantine/core'
+
+type ClientProps = {
+  name: string,
+  lastname: string,
+  username: string,
+  cedula: string
+}
+
+type TicketProps = {
+  place: number,
+  isSold: boolean,
+  soldTo?: ClientProps | undefined,
+}
+
+type ModalProps = {
+  tickets: TicketProps[]
+}
+
+function TicketModal({ tickets }: ModalProps) {
+  return (
+    <>
+      {
+        tickets.map((item, index) => {
+          return (
+            <Card>
+              <Text>{item.place}</Text>
+            </Card>
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default TicketModal
