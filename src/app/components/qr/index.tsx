0c5da2694a4a8a68ec90ea7c02305a66f75b185a
Javier Diaz <javierdiazt406@gmail.com>
2023-05-02 23:47:17 -0400

    Changing QR Code Generator

--- src/app/components/qr/index.tsx
+++ src/app/components/qr/index.tsx
@@ -1,43 +1,42 @@
 import QRCode from 'qrcode-generator';
 import { Svg, Path } from '@react-pdf/renderer'; 
 
-type QRProps = {
+type IQRCode = {
   value: string;
   type: 'react' | 'pdf';
   style?: React.CSSProperties;
 }
 
-function generarCodigoQR({ value, type, style }: QRProps) {
+function QRCodegenerator({ value, type, style }: IQRCode) {
   // Generar el codigo QR con la libreria qrcode-generator
   const qr = QRCode(0, 'H');
   qr.addData(value);
   qr.make();
 
   // Construir el SVG a partir del codigo QR
-  const tamaño = qr.getModuleCount() * 4;
-  const tamañoCelda = tamaño / qr.getModuleCount();
+  const size = qr.getModuleCount() * 4;
+  const cellSize = size / qr.getModuleCount();
   let path = '';
   for (let rowIndex = 0; rowIndex < qr.getModuleCount(); rowIndex++) {
     for (let cellIndex = 0; cellIndex < qr.getModuleCount(); cellIndex++) {
       if (qr.isDark(rowIndex, cellIndex)) {
-        path += `M${cellIndex * tamañoCelda},${rowIndex * tamañoCelda} `;
-        path += `h${tamañoCelda} v${tamañoCelda} h-${tamañoCelda} v-${tamañoCelda} `;
+        path += `M${cellIndex * cellSize},${rowIndex * cellSize}`;
+        path += `h${cellSize} v${cellSize} h-${cellSize} v-${cellSize} `;
       }
     }
   }
 
   return (
     type === 'pdf' ? (
-      <Svg viewBox={`0 0 ${tamaño} ${tamaño}`} style={{ width: '52px', height: '52px', marginLeft: '74px'}}>
+      <Svg viewBox={`0 0 ${size} ${size}`} style={{ width: '52px', height: '52px', marginLeft: '74px'}}>
         <Path d={path} fill="#000" />
       </Svg>
     ) : (
-      <svg viewBox={`0 0 ${tamaño} ${tamaño}`} style={style}>
+      <svg viewBox={`0 0 ${size} ${size}`} style={style}>
         <path d={path} fill="#000" />
       </svg>
     )
   )
-
 }
 
-export default generarCodigoQR;
+export default QRCodegenerator;
