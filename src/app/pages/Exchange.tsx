4bb3adf110c8a0f6b1c0dd3efae493160fd56469
Javier Diaz <javierdiazt406@gmail.com>
2023-05-17 10:14:42 -0400

    Exchanges changes

--- src/app/pages/Exchange.tsx
+++ src/app/pages/Exchange.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { links } from '../assets/data/links'
 import Navbar from '../components/navbar'
+import { Card, Grid } from '@mantine/core'
 
 interface IExchange {}
 
@@ -22,11 +23,25 @@ function Exchange({}: IExchange) {
       })
   }, [])
   return (
-    <div>
+    <>
       <Navbar profiles={profiles} links={links} />
-
-    </div>
-
+      <Grid mx={3} mt={5}>
+        <Grid.Col xs={12} md={6} lg={4}>
+          <Card shadow="sm" radius="sm">
+          </Card>
+        </Grid.Col>
+        <Grid.Col xs={12} md={6} lg={4}>
+          <Card shadow="sm" radius="sm">
+          </Card>
+        </Grid.Col>
+        <Grid.Col xs={12} md={6} lg={4}>
+          <Card shadow="sm" radius="sm">
+          </Card>
+        </Grid.Col>
+      </Grid>
+      <Card shadow="sm" radius="sm" mx={10} mt={15} h="100vh">
+      </Card>
+    </>
   )
 }
 
