46024e0b6dc212b0339f02436f7b8200f76cb0f1
EvananSemprun <evanansemprun@gmail.com>
2023-06-24 18:28:26 -0400

    draws

---
+++ src/app/components/table/drawtable.tsx
@@ -0,0 +1,269 @@
+import { useState } from 'react'
+import {
+  createStyles,
+  Table,
+  ScrollArea,
+  UnstyledButton,
+  Group,
+  Text,
+  Center,
+  TextInput,
+  Badge,
+} from '@mantine/core'
+import { keys } from '@mantine/utils'
+import { IconSelector, IconChevronDown, IconChevronUp, IconSearch } from '@tabler/icons-react'
+import { IconCheck, IconX } from '@tabler/icons'
+
+const useStyles = createStyles((theme) => ({
+  th: {
+    padding: '0 0 0 15px !important',
+  },
+
+  control: {
+    width: '100%',
+    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+  },
+
+  icon: {
+    width: '2.4rem',
+    height: '2.4rem',
+    borderRadius: '1.4rem',
+  },
+}))
+
+interface RowData {
+  title: string;
+  first_prize: string;
+  init_date:string;
+  expired_date:string;
+  draw_type:string;
+  limit:string;
+  price_unit:string;
+}
+
+interface TableSortProps {
+  data: RowData[];
+}
+
+
+interface ThProps {
+  children: React.ReactNode
+  reversed: boolean
+  sorted: boolean
+  onSort(): void
+}
+
+function Th({ children, reversed, sorted, onSort }: ThProps) {
+  const { classes } = useStyles()
+  const Icon = sorted ? (reversed ? IconChevronUp : IconChevronDown) : IconSelector
+  return (
+    <th className={classes.th}>
+      <UnstyledButton onClick={onSort} className={classes.control}>
+        <Group position="apart">
+          <Text fw={500} fz="md">
+            {children}
+          </Text>
+          <Center className={classes.icon}>
+            <Icon size="0.9rem" stroke={1.5} />
+          </Center>
+        </Group>
+      </UnstyledButton>
+    </th>
+  )
+}
+
+function filterData(data: RowData[], search: string) {
+  const query = search.toLowerCase().trim();
+  return data.filter((item) => 
+    item.first_prize.toLowerCase().includes(query) || 
+    item.title.toLowerCase().includes(query)  || 
+    item.init_date.toLowerCase().includes(query) || 
+    item.expired_date.toLowerCase().includes(query) || 
+    item.draw_type.toLowerCase().includes(query) || 
+    item.limit.toLowerCase().includes(query) || 
+    item.price_unit.toLowerCase().includes(query) 
+    
+  );
+}
+
+
+function sortData(
+  data: RowData[],
+  payload: { sortBy: keyof RowData | null; reversed: boolean; search: string }
+) {
+  const { sortBy } = payload
+
+  if (!sortBy) {
+    return filterData(data, payload.search)
+  }
+
+  return filterData(
+    [...data].sort((a, b) => {
+      if (payload.reversed) {
+        return b[sortBy].localeCompare(a[sortBy])
+      }
+
+      return a[sortBy].localeCompare(b[sortBy])
+    }),
+    payload.search
+  )
+}
+
+export default function TableSort({ data }: TableSortProps) {
+  const [search, setSearch] = useState('')
+  const [sortedData, setSortedData] = useState(data)
+  const [sortBy, setSortBy] = useState<keyof RowData | null>(null)
+  const [reverseSortDirection, setReverseSortDirection] = useState(false)
+
+  const setSorting = (field: keyof RowData) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false
+    setReverseSortDirection(reversed)
+    setSortBy(field)
+    setSortedData(sortData(data, { sortBy: field, reversed, search }))
+  }
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget
+    setSearch(value)
+    setSortedData(sortData(data, { sortBy, reversed: reverseSortDirection, search: value }))
+  }
+
+ function verifyPayload(payload: any) {
+  if (payload === "N/A") {
+    return <Text>Por anunciar</Text>;
+  }
+    switch (typeof payload) {
+      case 'string':
+      case 'number':
+        return <Text>{payload}</Text>;
+      case 'boolean':
+        if (payload === true) {
+          return (
+            <div style={{ display: 'flex', width: '100%' }}>
+              <div style={{ margin: 'auto' }}>
+                <Badge color="green">
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <IconCheck size="1rem" stroke={2} style={{ marginLeft: '-0.24rem' }} />
+                    <Text ml=".2rem" mt="-.1rem">Si</Text>
+                  </div>
+                </Badge>
+              </div>
+            </div>
+          );
+        } else {
+          return (
+            <div style={{ display: 'flex', width: '100%' }}>
+              <div style={{ margin: 'auto' }}>
+                <Badge color="red">
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <IconX size="1rem" stroke={2} style={{ marginLeft: '-0.24rem' }} />
+                    <Text ml=".2rem" mt="-.1rem">No</Text>
+                  </div>
+                </Badge>
+              </div>
+            </div>
+          );
+        }
+      case 'object':
+        if (payload === null) {
+          return <Text>Por anunciar</Text>;
+        }
+        if (Array.isArray(payload)) {
+          return <Text>{payload.join(', ')}</Text>;
+        }
+        if (payload instanceof Date) {
+          return <Text>{payload.toLocaleDateString()}</Text>;
+        }
+        return <Text>{JSON.stringify(payload)}</Text>;
+      case 'undefined':
+        
+      return <Text>Por anunciar</Text>;
+      default:
+        return <Text>{payload}</Text>;
+    }
+  };
+  const columnNames = [
+    "Título",
+    "Primer premio",
+    "Fecha de inicio",
+    "Fecha de expiración",
+    "Tipo de sorteo",
+    "Límite",
+    "Precio por ticket"
+  ];
+  
+  const rows = sortedData.map((row: RowData, index: number) => (
+    <tr key={index}>
+      <td>{row.title}</td>
+      <td>{row.first_prize}</td>
+      <td>{row.init_date}</td>
+      <td>{verifyPayload(row.expired_date)}</td>
+      <td>{row.draw_type}</td>
+      <td>{row.limit}</td>
+      <td>{row.price_unit}</td>
+    </tr>
+  ));
+  
+
+
+
+  return (
+    <ScrollArea>
+      {/* <TextInput
+        placeholder="Buscar por cualquier campo"
+        mb="md"
+        icon={<IconSearch size="1.3rem" stroke={2} />}
+        value={search}
+        size='md'
+        w={340}
+        radius={0}
+        onChange={handleSearchChange}
+      /> */}
+      <Table
+        striped
+        highlightOnHover
+        withColumnBorders
+        withBorder
+        horizontalSpacing="md"
+        verticalSpacing="sm"
+        miw={700}
+        sx={{ tableLayout: 'fixed' }}
+      >
+        <thead>
+          <tr>
+          {
+  keys(data[0]).map((key, index) => (
+    <Th
+      key={key}
+      sorted={sortBy === key}
+      reversed={reverseSortDirection}
+      onSort={() => setSorting(key)}
+    >
+      {columnNames[index]}
+    </Th>
+  ))
+}
+
+          </tr>
+        </thead>
+        <tbody>
+          {rows.length > 0 ? (
+            rows
+          ) : (
+            <tr>
+              <td colSpan={Object.keys(data[0]).length}>
+                <Text weight={700} my="8%" fz={20} align="center">
+                  No hay resultados
+                </Text>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </ScrollArea>
+  )
+}
--- src/app/pages/Draws.tsx
+++ src/app/pages/Draws.tsx
@@ -3,31 +3,62 @@ import axios from 'axios'
 import { links } from '../assets/data/links'
 import Navbar from '../components/navbar'
 import { Card, Stepper, Grid, Button, Modal, Title, Text } from '@mantine/core'
-import Table from '../components/table'
+import Table from '../components/table/drawtable'
 import DrawsModal from '../components/drawsModal';
-interface IDraws { }
 
-const jsonArray = [
-  {
-    "nombre": "Objeto 1",
-    "propiedad1": "Valor 1",
-    "propiedad2": "Valor 2"
-  },
-  {
-    "nombre": "Objeto 2",
-    "propiedad1": "Valor 3",
-    "propiedad2": "Valor 4"
-  },
-  {
-    "nombre": "Objeto 3",
-    "propiedad1": "Valor 5",
-    "propiedad2": "Valor 6"
-  }
-];
+interface IDraws { }
 
 function Draws({ }: IDraws) {
   const [profiles, setProfiles] = useState([])
   const [openForm, setOpenForm] = useState(false)
+  const [draws, setDraws] = useState<{ 
+    title: string ;
+    first_prize: string ;
+    init_date: string ;
+    expired_date:string ;
+    draw_type:string ;
+    limit:string ;
+    price_unit:string ;
+    current: number;
+  }[]>([]);
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios
+    .post(
+      'http://137.184.93.36/api/public/draws',
+      {
+        user_id: localStorage.getItem('user_id') || 1,
+      },
+      {
+        headers: {
+          'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzeXN0ZW0iOiJyaWZhbWF4Iiwic2VjcmV0IjoiZjJkN2ZhNzE3NmE3NmJiMGY1NDI2ODc4OTU5YzRmNWRjMzVlN2IzMWYxYzE1MjYzNThhMDlmZjkwYWE5YmFlMmU4NTc5NzM2MDYzN2VlODBhZTk1NzE3ZjEzNGEwNmU1NDIzNjc1ZjU4ZDIzZDUwYmI5MGQyNTYwNjkzNDMyOTYiLCJoYXNoX2RhdGUiOiJNb24gTWF5IDI5IDIwMjMgMDg6NTE6NTggR01ULTA0MDAgKFZlbmV6dWVsYSBUaW1lKSJ9.ad-PNZjkjuXalT5rJJw9EN6ZPvj-1a_5iS-2Kv31Kww`,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      }
+    )
+    .then((res) => {
+      const drawsData = res.data.map((draw: any) => ({
+        title: draw.title,
+        first_prize: draw.first_prize,
+        init_date: draw.init_date,
+        expired_date: draw.expired_date,
+        draw_type: draw.draw_type,
+        limit: draw.limit,
+        price_unit: draw.price_unit,
+        current: draw.progress.current 
+      }));
+      setDraws(drawsData);
+      setLoading(false);
+    })
+    
+    .catch((err) => {
+      console.log(err);
+    });
+  
+  }, [])
 
   useEffect(() => {
     axios.get('https://rifa-max.com/api/v1/riferos', {
@@ -49,7 +80,7 @@ function Draws({ }: IDraws) {
 
   const handleOpen = () => {
     setOpenForm(true)
-  } 
+  }
 
   const handleClose = () => {
     setOpenForm(false)
@@ -60,23 +91,18 @@ function Draws({ }: IDraws) {
       <Navbar profiles={profiles} links={links} />
 
       <Card mx={15} mt={15} shadow={"0 0 7px 0 #5f5f5f3d"}>
-
         <Grid>
-          
           <Grid.Col md={5} sm={12}>
-
             <Title order={2} fw={500} mb={20}>
               Operadoras
-
               <Text fw={300} fz={20} mb={-7}>
                 Reportes de las rifas de motos realizadas en el mes
               </Text>
             </Title>
           </Grid.Col>
-
           <Grid.Col md={7} sm={12}>
             <DrawsModal
-              variant="filled"
+              variant="filled"  
               color="blue"
               style={{ float: "right" }}
               className="btn-rifa"
@@ -88,13 +114,14 @@ function Draws({ }: IDraws) {
             </DrawsModal>
           </Grid.Col>
         </Grid>
-        <Table
-          data={jsonArray}
-        />
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <Table data={draws} />
+        )}
       </Card>
     </>
-
   )
 }
 
-export default Draws
+export default Draws;
