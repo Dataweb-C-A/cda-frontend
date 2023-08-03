4a94e58d94236d2ba0475d57e58340bf8c25ed8b
EvananSemprun <evanansemprun@gmail.com>
2023-08-03 17:27:38 -0400

    all filters and pagination

--- src/app/pages/Reporterifa.tsx
+++ src/app/pages/Reporterifa.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-
 import { Link, useHistory } from "react-router-dom";
 import { Grid, TextInput, Table, Group, Pagination, Select, Card, Avatar, Text, Title, useMantineTheme } from '@mantine/core'
 import Cards from '../components/cards'
@@ -20,6 +19,25 @@ function Reporterifa({ }: Props) {
   }
 
   const [elements, setElements] = useState<Elemento[]>([]);
+  const [filteredElements, setFilteredElements] = useState<Elemento[]>(elements);
+  const [resultCommisssion, setResultCommisssion] = useState('');
+  const [commissionPercentage, setCommissionPercentage] = useState('');
+  const [todayEarnings, setTodayEarnings] = useState('');
+  const [selectOptions, setSelectOptions] = useState<{ value: string; label: string }[]>([]);
+  const [users, setUsers] = useState<any>([])
+  const [profiles, setProfiles] = useState([])
+  const [stats, setStats] = useState<any>({})
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const history = useHistory();
+  const [searchInput, setSearchInput] = useState('');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 14;
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   const ths = (
     <tr>
       <th>Premio</th>
@@ -29,23 +47,10 @@ function Reporterifa({ }: Props) {
     </tr>
   );
 
-  const [filteredElements, setFilteredElements] = useState<Elemento[]>(elements);
-  const [dateFrom, setDateFrom] = useState<Date | null>(null);
-  const [dateTo, setDateTo] = useState<Date | null>(null);
-  const [resultCommisssion, setResultCommisssion] = useState('');
-  const [commissionPercentage, setCommissionPercentage] = useState('');
-  const [todayEarnings, setTodayEarnings] = useState(''); 
-
-  const [premioFilter, setPremioFilter] = useState<string>(''); // Estado para el filtro por título
-
-
-
-  const [selectOptions, setSelectOptions] = useState<{ value: string; label: string }[]>([]);
-  const [selectedOption, setSelectedOption] = useState('');
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem('user') || '{}');
     const apiUrl = `https://api.rifamax.app/places/reports?agency_id=${user.id}`;
-  
+
     axios
       .get(apiUrl)
       .then((response) => {
@@ -53,9 +58,9 @@ function Reporterifa({ }: Props) {
         setElements(data.places);
         setFilteredElements(data.places);
         setCommissionPercentage(data.ui.commission_parser);
-        setResultCommisssion(data.ui.earnings.result_commission);  
+        setResultCommisssion(data.ui.earnings.result_commission);
         setTodayEarnings(data.ui.earnings.today_earnings_parser);
-        
+
         const uniqueTitles = Array.from(new Set<string>(data.places.map((element: Elemento) => element.title)));
         const options = uniqueTitles.map((title) => ({ value: title, label: title }));
         setSelectOptions(options);
@@ -64,57 +69,48 @@ function Reporterifa({ }: Props) {
         console.error('Error fetching data:', error);
       });
   }, []);
-
-  const rows = filteredElements.map((element) => (
-    <tr key={element.title}>
-      <td>{element.title}</td>
-      <td>{element.sold_at.slice(0, 10)}</td>
-      <td>{`${element.price_unit}$`}</td>
-      <td>{`${element.ganancia_final}$`}</td>
-    </tr>
-  ));
-
-
-
-
-  const handleSelectChange = (selected: { value: string; label: string }) => {
-    setSelectedOption(selected.value);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, filteredElements.length);
+  const paginatedRows = filteredElements
+    .slice(startIndex, endIndex)
+    .map((element, index) => (
+      <tr key={index}>
+        <td>{element.title}</td>
+        <td>{element.sold_at.slice(0, 10)}</td>
+        <td>{`${element.price_unit}$`}</td>
+        <td>{`${element.ganancia_final}$`}</td>
+      </tr>
+    ));
+  const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
   };
- 
-const filterElements = () => {
-  const filtered = elements.filter((element) => {
-    const premioIncludesFilter =
-      premioFilter === '' || element.title.toLowerCase().includes(premioFilter.toLowerCase());
 
-    if (dateFrom && dateTo) {
-      
-      const adjustedDateFrom = new Date(Date.UTC(dateFrom.getFullYear(), dateFrom.getMonth(), dateFrom.getDate()));
-      const adjustedDateTo = new Date(Date.UTC(dateTo.getFullYear(), dateTo.getMonth(), dateTo.getDate()));
+  const toUTC = (date: Date) => {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+  };
 
-      const vendidoaDate = new Date(element.sold_at);
-      vendidoaDate.setUTCHours(0, 0, 0, 0);
+  const filterElements = () => {
+    const filteredByTitle = elements.filter((element) =>
+      element.title.toLowerCase().includes(searchInput.toLowerCase())
+    );
 
-      const dateInRange = vendidoaDate >= adjustedDateFrom && vendidoaDate <= adjustedDateTo;
-      return premioIncludesFilter && dateInRange;
-    }
+    const filteredByDate = filteredByTitle.filter((element) => {
+      if (startDate && endDate) {
+        const soldAtDate = toUTC(new Date(element.sold_at));
+        const startUTC = toUTC(startDate);
+        const endUTC = toUTC(endDate);
+        return soldAtDate >= startUTC && soldAtDate <= endUTC;
+      }
+      return true;
+    });
 
-    return premioIncludesFilter;
-  });
+    setFilteredElements(filteredByDate);
+  };
 
-  setFilteredElements(filtered);
-};
+  useEffect(() => {
+    filterElements();
+  }, [searchInput, startDate, endDate, elements]);
 
-useEffect(() => {
-  filterElements();
-}, [dateFrom, dateTo, premioFilter]);
-
-
-
-  const [users, setUsers] = useState<any>([])
-  const [profiles, setProfiles] = useState([])
-  const [stats, setStats] = useState<any>({})
-
-  const history = useHistory();
   useEffect(() => {
     axios.get('https://rifa-max.com/api/v1/rifas/stats', {
       headers: {
@@ -175,10 +171,9 @@ useEffect(() => {
             label='Ganancia total'
           />
         </Grid.Col>
-
       </Grid>
 
-      <Card shadow="sm" radius="sm" mx={15} mt={5} h="100vh">
+      <Card shadow="sm" radius="sm" mx={15} mt={5} h="80vh">
         <Grid>
           <Grid.Col xl={6} md={6} xs={12}>
             <Title order={2} fw={500} mb={20}>
@@ -190,7 +185,13 @@ useEffect(() => {
           </Grid.Col>
           <Grid.Col xl={6} md={6} xs={12}>
             <Group position='right'>
-              <Select label="filtre por premio" data={selectOptions} />
+              <TextInput
+                mt={-10}
+                placeholder="Escribe el premio"
+                label="Filtrar por premio"
+                value={searchInput}
+                onChange={handleSearchInputChange}
+              />
               {/**fecha de inicio */}
               <DatePicker
                 mt={-10}
@@ -198,8 +199,8 @@ useEffect(() => {
                 inputFormat="YYYY MMM DD"
                 label="Filtrar desde"
                 variant="filled"
-                value={dateFrom}
-                onChange={(value) => setDateFrom(value)}
+                value={startDate}
+                onChange={(date) => setStartDate(date)}
               />
 
               <DatePicker
@@ -208,29 +209,21 @@ useEffect(() => {
                 inputFormat="YYYY MMM DD"
                 label="Filtrar hasta"
                 variant="filled"
-                value={dateTo}
-                onChange={(value) => setDateTo(value)}
+                value={endDate}
+                onChange={(date) => setEndDate(date)}
               />
-
-
             </Group>
           </Grid.Col>
         </Grid>
-        {/**
-         * <Pagination
-        total={Math.ceil(elements.length / elementsPerPage)}
-        initialPage={currentPage}
-        onPageChange={handlePageChange}
-        boundaryLinks
-        size="sm"
-        rtl
-      />
-         * 
-         */}
+        <Pagination
+          total={Math.ceil(filteredElements.length / itemsPerPage)} 
+          onChange={handlePageChange} 
+        />
 
         <Table mt={15} captionSide="bottom" withColumnBorders highlightOnHover>
           <thead>{ths}</thead>
-          <tbody>{rows}</tbody>
+          <tbody>{paginatedRows}
+          </tbody>
         </Table>
       </Card>
     </>
