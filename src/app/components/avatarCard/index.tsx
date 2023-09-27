422208ca1734fc7acba7249f60d56c8444394846
EvananSemprun <evanansemprun@gmail.com>
2023-09-27 10:44:44 -0400

    games edit

--- src/app/components/avatarCard/index.tsx
+++ src/app/components/avatarCard/index.tsx
@@ -1,64 +1,89 @@
-import { useState } from 'react'
-import { Text, Avatar, Card, Modal, Group  } from '@mantine/core'
-import '../../assets/scss/avatarCard.scss'
-import { BsPencil as Pencil } from 'react-icons/bs'
-import { DropArea } from '../droparea'
+import React, { useState } from 'react';
+import { Text, Avatar, Card, Modal, Image, Group, Title } from '@mantine/core';
+import '../../assets/scss/avatarCard.scss';
+import { BsPencil as Pencil } from 'react-icons/bs';
+import { IconCheck, IconX } from '@tabler/icons-react';
+import RifamaxLogo from "../../assets/images/rifamax-logo.png";
 
 type AvatarProps = {
-  image?: string | ''
-  name: string
-  role: string
-  cedula?: string
-  border?: boolean | false
-  width?: number | undefined
-  margin?: number | undefined
-  padding?: number | undefined
-  children?: React.ReactNode
-  hasHover?: boolean | false
-  style?: React.CSSProperties
+  image?: string | '';
+  name: string;
+  role: string;
+  cedula?: string;
+  border?: boolean | false;
+  width?: number | undefined;
+  margin?: number | undefined;
+  padding?: number | undefined;
+  children?: React.ReactNode;
+  hasHover?: boolean | false;
+  style?: React.CSSProperties;
+};
+interface juegosProps {
+  src: string;
 }
 
 const getInitials = (name: string) => {
-  const names = name.split(' ')
-  if (names.length === 1) return names[0].charAt(0)
-  return `${names[0].charAt(0)}${names[names.length - 1].charAt(0)}`.toUpperCase()
+  const names = name.split(' ');
+  if (names.length === 1) return names[0].charAt(0);
+  return `${names[0].charAt(0)}${names[names.length - 1].charAt(0)}`.toUpperCase();
+};
+
+function TarjetaConAlternanciaDeIconos({ src }: juegosProps) {
+  const [icono, setIcono] = useState('check');
+
+  const alternarIcono = () => {
+    setIcono(icono === 'check' ? 'x' : 'check');
+  };
+
+  return (
+    <Card style={{ width: 240, height: 190 }} onClick={alternarIcono}>
+      <Group position='right'>
+        {icono === 'check' ? (
+          <IconCheck color='green' size={18} />
+        ) : (
+          <IconX color='red' size={18} />
+        )}
+      </Group>
+      <Image radius="md" src={src} alt="Random unsplash image" />
+    </Card>
+  );
 }
 
-function AvatarCard({name, image, role, border, width, padding, margin, style, children, hasHover}: AvatarProps) {
-  const [isHovering, setIsHovering] = useState(false)
-  const [editModal, setEditModal] = useState(false)
+function AvatarCard({ name, image, role, border, width, padding, margin, style, children, hasHover }: AvatarProps) {
+  const [isHovering, setIsHovering] = useState(false);
+  const [editModal, setEditModal] = useState(false);
 
   return (
     <div
       onClick={() => setEditModal(true)}
       onMouseEnter={() => {
-        true && setIsHovering(true)
+        true && setIsHovering(true);
       }}
       onMouseLeave={() => setIsHovering(false)}
     >
-      <Card 
-        shadow="sm" 
-        p={padding} 
-        w={width} 
-        withBorder={border} 
-        m={margin} 
+      <Card
+        shadow="sm"
+        p={padding}
+        w={width}
+        withBorder={border}
+        m={margin}
         style={style}
       >
         <div className="avatar-card">
-          <Avatar 
+          <Avatar
             src={image}
             size="lg"
             radius="md"
             style={{ filter: isHovering ? 'blur(5px)' : 'none' }}
             onMouseEnter={() => {
-              true && setIsHovering(true)
+              true && setIsHovering(true);
             }}
             onMouseLeave={() => setIsHovering(false)}
           >
             {getInitials(name)}
           </Avatar>
           {true && (
-            <div 
+            <div
               className="pencil-overlay"
             >
               <Pencil />
@@ -66,7 +91,7 @@ function AvatarCard({name, image, role, border, width, padding, margin, style, c
           )}
           <Text>
             <strong>{name}</strong>
-            <br/>
+            <br />
             {role}
           </Text>
         </div>
@@ -75,14 +100,18 @@ function AvatarCard({name, image, role, border, width, padding, margin, style, c
       <Modal
         opened={editModal}
         onClose={() => setEditModal(false)}
-        title={`ELIGAR JUEGO`}
+        size="xl"
+        title={`ELEGIR JUEGO`}
       >
         <Group position="center">
-         
+          <TarjetaConAlternanciaDeIconos src={RifamaxLogo} />
+          <TarjetaConAlternanciaDeIconos src={RifamaxLogo} />
+          <TarjetaConAlternanciaDeIconos src={RifamaxLogo} />
+
         </Group>
       </Modal>
     </div>
-  )
+  );
 }
 
-export default AvatarCard
+export default AvatarCard;
