e49feb91d3b19fa207e76c1e35ccab3dd62aa752
EvananSemprun <evanansemprun@gmail.com>
2023-10-18 16:25:59 -0400

    beer 2

--- src/app/pages/Pot.tsx
+++ src/app/pages/Pot.tsx
@@ -37,47 +37,48 @@ function Pot({ }: Props) {
         "imagen": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Estadio_M%C3%A2s_Monumental.jpg",
         "titulo": "Partido 1",
         "fecha": "2023-10-17",
-        "tickets_vendidos":1000,
-        "monto_recaudado":3000,
+        "tickets_vendidos": 1000,
+        "monto_recaudado": 35000,
         "pote": 1000.50,
         "piggy_acc": 50
-      },
-      {
-        "imagen": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Estadio_M%C3%A2s_Monumental.jpg",
-        "titulo": "Partido 2",
-        "fecha": "2023-10-18",
-        "tickets_vendidos":1000,
-        "monto_recaudado":10000,
-        "pote": 3050.25,
-        "piggy_acc": 100
-      },
-      {
-        "imagen": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Estadio_M%C3%A2s_Monumental.jpg",
-        "titulo": "Partido 3",
-        "fecha": "2023-10-19",
-        "tickets_vendidos":1000,
-        "monto_recaudado":14000,
-        "pote": 8200.75,
-        "piggy_acc": 12
-      },
-
-      {
-        "imagen": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Estadio_M%C3%A2s_Monumental.jpg",
-        "titulo": "Partido 3",
-        "fecha": "2023-10-19",
-        "tickets_vendidos":1000,
-        "monto_recaudado":50000,
-        "pote": 10200.75,
-        "piggy_acc": 12
       }
+      // ,
+      // {
+      //   "imagen": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Estadio_M%C3%A2s_Monumental.jpg",
+      //   "titulo": "Partido 2",
+      //   "fecha": "2023-10-18",
+      //   "tickets_vendidos":1000,
+      //   "monto_recaudado":10000,
+      //   "pote": 3050.25,
+      //   "piggy_acc": 100
+      // },
+      // {
+      //   "imagen": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Estadio_M%C3%A2s_Monumental.jpg",
+      //   "titulo": "Partido 3",
+      //   "fecha": "2023-10-19",
+      //   "tickets_vendidos":1000,
+      //   "monto_recaudado":14000,
+      //   "pote": 8200.75,
+      //   "piggy_acc": 12
+      // },
+
+      // {
+      //   "imagen": "https://upload.wikimedia.org/wikipedia/commons/0/0f/Estadio_M%C3%A2s_Monumental.jpg",
+      //   "titulo": "Partido 3",
+      //   "fecha": "2023-10-19",
+      //   "tickets_vendidos":1000,
+      //   "monto_recaudado":50000,
+      //   "pote": 10200.75,
+      //   "piggy_acc": 12
+      // }
     ],
     "Aparicio": [
       {
         "imagen": "https://media.noticiaalminuto.com/2019/08/estadio-luis-aparicio.jpg",
         "titulo": "Partido 1",
         "fecha": "2023-10-17",
-        "tickets_vendidos":1000,
-        "monto_recaudado":10000,
+        "tickets_vendidos": 1000,
+        "monto_recaudado": 10000,
         "pote": 1000.50,
         "piggy_acc": 12
       },
@@ -85,8 +86,8 @@ function Pot({ }: Props) {
         "imagen": "https://media.noticiaalminuto.com/2019/08/estadio-luis-aparicio.jpg",
         "titulo": "Partido 2",
         "fecha": "2023-10-18",
-        "tickets_vendidos":1000,
-        "monto_recaudado":10000,
+        "tickets_vendidos": 1000,
+        "monto_recaudado": 10000,
         "pote": 7650.25,
         "piggy_acc": 12
       }
@@ -118,9 +119,9 @@ function Pot({ }: Props) {
         {selectedLocalidadData.map((partido, index) => (
           <Card
             key={index}
-            w={320}
-            mt={15}
-            h={350}
+            w={750}
+            mt={55}
+            h={600}
             radius="md"
             shadow="sm"
             withBorder
@@ -128,7 +129,7 @@ function Pot({ }: Props) {
             <Card.Section>
               <Image
                 src={partido.imagen}
-                height={160}
+                height={260}
                 alt={`Imagen del partido ${partido.titulo}`}
               />
             </Card.Section>
@@ -138,24 +139,22 @@ function Pot({ }: Props) {
                 {partido.titulo}
               </Title>
             </Group>
+            <Group position="apart">
 
-            <Text mt={5} weight={500}>
-              Fecha: {partido.fecha}
-            </Text>
+              <Text mt={5} >
+                Fecha: {partido.fecha}
+              </Text>
 
-            <Text mt={10} weight={500}>
-              Tickets vendidos: {partido.tickets_vendidos}
-            </Text>
+              <Text mt={10} >
+                Tickets vendidos: {partido.tickets_vendidos}
+              </Text>
 
-            <Text mt={10} weight={500}>
-              Monto a repartir: {partido.monto_recaudado * 0.5}
-            </Text>
+              <Text mt={10} >
+                Monto a repartir: {partido.monto_recaudado * 0.5}
+              </Text>
+            </Group>
 
-            
 
-            <div style={{ width: 240, marginLeft: 270, marginTop: 30 }}>
-
-            
 
             {/* <ActionIcon mt={-53} ml={-49} w={80} variant="transparent" style={{ color: "inherit" }}>
 
@@ -163,26 +162,30 @@ function Pot({ }: Props) {
             <IconPig size="180px" />
             </ActionIcon>  */}
 
+            <div style={{ marginLeft: 230 }}>
 
-              <ActionIcon ml={-52} w={100} variant="transparent" mt={-50} mb={-10} style={{ color: (partido.monto_recaudado * 0.5) >= 10000 ? "#1d870c" : "inherit" }}  >
-                <IconRectangleFilled size="40px" />
+
+             
+
+              <ActionIcon ml={1} w={250} mt={55} variant="transparent" style={{ color: (partido.monto_recaudado * 0.5) >= 10000 ? "#1d870c" : "inherit" }}  >
+                <IconRectangleFilled size="150px" />
               </ActionIcon>
 
-              <ActionIcon ml={-45} w={85} variant="transparent" mb={-15} style={{ color: (partido.monto_recaudado * 0.5) >= 7000 ? "#1d870c" : "inherit" }} >
-                <IconRectangleFilled size="40px" />
+               <ActionIcon ml={1} w={250} variant="transparent" style={{ color: (partido.monto_recaudado * 0.5) >= 3000 || (partido.monto_recaudado * 0.5) <= 7000 ? "#1d870c" : "inherit" }} >
+                <IconRectangleFilled size="95px" />
 
               </ActionIcon>
 
-              <ActionIcon ml={-45} w={85} variant="transparent" mb={-15} style={{ color: (partido.monto_recaudado * 0.5) >= 3000 ? "#1d870c" : "inherit" }} >
-                <IconRectangleFilled />
+
+{/*
+              <ActionIcon ml={25} w={200} mt={40} variant="transparent" style={{ color: (partido.monto_recaudado * 0.5) < 3000 ? "#1d870c" : "inherit" }} >
+                <IconRectangleFilled size="95px" />
+              </ActionIcon> */}
+
+              <ActionIcon ml={-99} mt={40} w={450} variant="transparent" style={{ color: "inherit" }}>
+                <IconBeer size="295px" />
               </ActionIcon>
 
-              <ActionIcon ml={-45} w={85} variant="transparent" style={{ color: (partido.monto_recaudado * 0.5) >= 1000 ? "#1d870c" : "inherit" }} >
-                <IconRectangleFilled />
-              </ActionIcon>
-              <ActionIcon mt={-53} ml={-42} w={80} variant="transparent" style={{ color: "inherit" }}>
-                <IconBeer size="180px" />
-              </ActionIcon>
             </div>
           </Card>
 
