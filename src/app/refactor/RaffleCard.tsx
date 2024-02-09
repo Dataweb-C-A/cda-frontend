e85a5d24b6131addb2d9ac8d48632a0014901004
evanansemprun <evanansemprun@gmail.com>
2024-02-09 14:29:37 -0400

    x100 100 tickest

--- src/app/refactor/RaffleCard.tsx
+++ src/app/refactor/RaffleCard.tsx
@@ -14,40 +14,66 @@ interface IRaffleCard {
 function RaffleCard({ data, progress, style, className, onClick }: IRaffleCard) {
   const theme = useMantineTheme()
   return (
-    <Card
-      mb={10}
-      ml={10}
-      mt={10}
-      onClick={onClick}
-      withBorder
-      style={style}
-      className={className}
-    >
-      <div style={{ position: "absolute", top: 0, right: 0 }}>
-        <Group spacing={0}>
-          <Title fw={400} fz={12.5} style={{ padding: '5px', background: '#D24545', borderRadius: '0 0 0 5px', boxShadow: '5px' }}>
-            {data.expired_date == null ? "Por definir" : moment(data.expired_date).format('DD/MM/YYYY')}
+    <>
+      <Card
+        ml={10}
+        mt={10}
+        onClick={onClick}
+        withBorder
+        style={style}
+        className={className}
+        pb={0}
+      >
+        {/* <div style={{ position: "absolute", top: 0, right: 0 }}>
+          <Group spacing={0}>
+            <Title fw={400} fz={12.5} style={{ padding: '5px', background: '#D24545', borderRadius: '0 0 0 5px', boxShadow: '5px' }}>
+              {data.expired_date == null ? "Por definir" : moment(data.expired_date).format('DD/MM/YYYY')}
+            </Title>
+            <Title fw={400} mt={-1.5} fz={12.5} style={{ padding: '5px', background: '#E67700', borderRadius: '0 0 0 0', boxShadow: '5px' }}>
+              {data.tickets_count} Números
+            </Title>
+            <Title fw={400} fz={12.5} style={{ padding: '5px', background: '#2B8A3E', borderRadius: '0 0 5px 0', boxShadow: '5px' }}>
+              Rifa {data.draw_type}
+            </Title>
+          </Group>
+        </div> */}
+
+        <Group>
+          <Title fw={400} fz={16.5}>
+            Rifa de :
           </Title>
-          <Title fw={400} mt={-1.5} fz={12.5} style={{ padding: '5px', background: '#E67700', borderRadius: '0 0 0 0', boxShadow: '5px' }}>
-            {data.tickets_count} Números
-          </Title>
-          <Title fw={400} fz={12.5} style={{ padding: '5px', background: '#2B8A3E', borderRadius: '0 0 5px 0', boxShadow: '5px' }}>
-            Rifa {data.draw_type}
+          <Title fw={400} fz={16.5}>
+            {data.title}
           </Title>
         </Group>
-      </div>
-      <Title mt={15} pb={5} fw={400} fz={16.5}>
-        {data.title}
-      </Title>
-       <Text
-        fz={11.5}
-       >
-       <strong>Inicio:</strong> {moment(data.init_date).format('DD/MM/YYYY hh:mm')}
-       </Text>
-      <Text fw={300} fz={14}>
-      </Text>
-      <Progress label={`${progress}%`} color={theme.colors.green[8]} mt={5} size='lg' value={progress < 1 ? 13 : progress > 1 ? 17 : progress} />
-    </Card>
+
+        <Group>
+          <Title fw={400} fz={16.5}>
+            Tipo de rifa :
+          </Title>
+          <Title fw={400} fz={16.5}>
+            {data.tickets_count} Números
+          </Title>
+        </Group>
+        <Group>
+          <Title fw={400} fz={16.5}>
+            Fecha sorteo
+          </Title>
+          <Title fw={400} fz={15}>
+            {moment(data.init_date).format('DD/MM/YYYY hh:mm')}
+          </Title>
+        </Group>
+        <Group>
+          <Title fw={400} fz={16.5}>
+            Loteria :
+          </Title>
+          <Title fw={400} fz={16.5}>
+          Zulia 7a 7:05 Pm
+          </Title>
+        </Group>
+      </Card>
+      <Progress label={`${progress}%`} color={theme.colors.green[8]} mt={0} size='lg' value={progress < 1 ? 13 : progress > 1 ? 17 : progress} />
+    </>
   );
 }
 
