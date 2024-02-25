aac443d9a5a4498676b9fb1ad55c2545c277a0f3
Javier Diaz <javierdiazt406@gmail.com>
2024-02-25 14:36:27 -0400

    ServiceWorker

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteSvgr from "vite-plugin-svgr";
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
